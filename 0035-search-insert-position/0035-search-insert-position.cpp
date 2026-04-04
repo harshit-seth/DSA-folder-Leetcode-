9162c7042f0acd3d0b64b551197e64843c4f69ab
Harshit Seth <harshitseth7084@gmail.com>
2026-04-04 22:59:06 +0530

    Time: 0 ms (100%), Space: 13.6 MB (42.2%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -6,13 +6,12 @@ public:
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid]  > target) {
-                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1; // right jao
             } else {
-                start = mid + 1;
+                end = mid - 1;
             }
         }
         return start;
