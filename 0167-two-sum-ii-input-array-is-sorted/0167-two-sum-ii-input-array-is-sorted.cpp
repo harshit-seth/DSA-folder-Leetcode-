2708b7304d1f16535516fd76cd65996bb39aaf08
Harshit Seth <harshitseth7084@gmail.com>
2026-09-02 20:52:21 +0530

    Time: 0 ms (100%), Space: 19.5 MB (33.92%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -8,7 +8,9 @@ public:
             int total = numbers[left] + numbers[right];
             if (total == target) {
                 return {left + 1, right + 1};
-            } else if (total > target) {
+            }
+
+            else if (total > target) {
                 right--;
             } else {
                 left++;
